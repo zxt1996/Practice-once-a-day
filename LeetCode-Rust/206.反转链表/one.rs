ca0f63d1d0fed2c793ee86c9ebfa4f5811b24910
zxt1996 <15625144271@163.com>
2021-02-24 00:04:27 +0800

    链表

---
+++ LeetCode-Rust/206.反转链表/one.rs
@@ -0,0 +1,30 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut pre = None;
+        let mut cur = head;
+
+        while let Some(mut value) = cur {
+            cur = value.next.take();
+            value.next = pre;
+            pre = Some(value);
+        }
+
+        pre
+    }
+}
