71363d46ddcef219e51c19d65e6d55bca4c6d2e1
zxt1996 <15625144271@163.com>
2021-02-27 23:09:38 +0800

    设计链表

---
+++ LeetCode-JS/707.设计链表/one.js
@@ -0,0 +1,74 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyLinkedList = function() {
+    this.allVal = [];
+    this.val = 0;
+    this.next = this.val + 1;
+};
+
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1. 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    if (index < 0 || index > this.allVal.length - 1) {
+        return -1;
+    } else {
+        return this.allVal[index];
+    }
+};
+
+/**
+ * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    this.allVal.unshift(val);
+};
+
+/**
+ * Append a node of value val to the last element of the linked list. 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+    this.allVal.push(val);
+};
+
+/**
+ * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    if (index < 0) {
+       this.allVal.unshift(val);
+    } else if (index > this.allVal.length - 1) {
+        this.allVal.push(val);
+    } else {
+        this.allVal.splice(index, 0, val);
+    }
+};
+
+/**
+ * Delete the index-th node in the linked list, if the index is valid. 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    this.allVal.splice(index, 1);
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
