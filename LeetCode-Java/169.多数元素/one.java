2e76480295886ce73be9d0446a812bc98e92ea08
zxt1996 <15625144271@163.com>
2021-07-21 21:04:56 +0800

    哈希

---
+++ LeetCode-Java/169.多数元素/one.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i]) > nums.length / 2) return nums[i];
+        }
+
+        return -1;
+    }
+}
