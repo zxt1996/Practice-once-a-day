dd8a9fac2f14f819fa1c155d19ab8546ec69db42
zxt1996 <15625144271@163.com>
2021-11-27 10:50:55 +0800

    feat: 哈希表

---
+++ LeetCode-C/350.两个数组的交集II/one.c
@@ -0,0 +1,46 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct {
+    int key;
+    int count;            
+    UT_hash_handle hh;
+} Hash;
+
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
+    Hash *hash = NULL;
+    int* res = (int*)malloc(sizeof(int) * nums1Size);
+    *returnSize = 0;
+
+    for (int i = 0; i < nums1Size; i++) {
+        Hash *s;
+
+        HASH_FIND_INT(hash, &nums1[i], s);
+
+        if (s != NULL) {
+            s->count += 1;
+        } else {
+            s = malloc(sizeof(Hash));
+            s->key = nums1[i];
+            s->count = 1;
+
+            HASH_ADD_INT(hash, key, s);
+        }
+    }
+
+    for (int i = 0; i < nums2Size; i++) {
+        Hash *s;
+        s = malloc(sizeof(Hash));
+        s->key = nums2[i];
+
+        HASH_FIND_INT(hash, &nums2[i], s);
+
+        if (s != NULL && s->count > 0) {
+            res[(*returnSize)++] = nums2[i];
+
+            s->count -= 1;
+        }
+    }
+
+    return res;
+}
