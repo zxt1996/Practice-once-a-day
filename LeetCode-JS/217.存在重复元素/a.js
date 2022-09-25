0b7d4ea37acdf73390a884c79ad97cb39e57cfb9
zxt1996 <15625144271@163.com>
2022-09-25 09:12:40 +0800

    feat: 哈希表

---
+++ LeetCode-JS/217.存在重复元素/a.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return true;
+        } else {
+            map.set(nums[i], 1);
+        }
+    }
+
+    return false;
+};
