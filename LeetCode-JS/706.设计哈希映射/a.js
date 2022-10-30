ad84988dc7917a39e9cb191f072cc5bce0943b52
zxt1996 <15625144271@163.com>
2022-10-30 11:05:11 +0800

    feat: 哈希映射

---
+++ LeetCode-JS/706.设计哈希映射/a.js
@@ -0,0 +1,40 @@
+var MyHashMap = function() {
+    this.mapObj = {};
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.mapObj[key] = value;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    if (key in this.mapObj) {
+        return this.mapObj[key];
+    }
+
+    return -1;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    delete this.mapObj[key];
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
